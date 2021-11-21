790b2986863ac659b56b96078af36cba78e4b336
MoElaSec <M.CS.Dev@protonmail.com>
2021-11-21 13:32:28 +0100

    Lab3 done 🚀

--- 3.PHP Control Structures If else, Switch Case/Lab3_Controle_flow.php
+++ 3.PHP Control Structures If else, Switch Case/Lab3_Controle_flow.php
@@ -1,30 +1,26 @@
 <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>3'rd Assignment: Flow Control</h1>
 
-<html>
+        <table  align="center" style="width:85%" cellpadding="5" cellspacing="4">
+            <?php 
 
-<body>
-	<h1>3'rd Assignment: Flow Control</h1>
-
-    <table  align="left" border="1" cellpadding="3" cellspacing="0">
-        <?php 
-            for ($i = 1; $i < 6+1; $i++) {
-                echo "<tr>";
-                for ($j = 1; $j < 5+1; $j++) {
-                    echo "<td>$i * $j = " . $i*$j . "</td>";
+                for ($i = 1; $i < 10; $i++) {
+                    echo "<tr>";
+                    for ($j = 1; $j < 10; $j++) {
+                        echo "<td>" . $i*$j . "</td>";
+                    }
+                    echo "</tr>";
                 }
-                echo "</tr>";
-            }
-        ?>
-    </table>
-</body>
+            ?>
+        </table>
 
+    </body>
 </html>
-
-
-<!--  
-    *
-    * *
-    * * *
-    * * * *
-    * * * * *
-  -->
