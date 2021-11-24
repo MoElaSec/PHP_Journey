f205a68f84f494e4924e09ff0eaadba24989574e
MoElaSec <M.CS.Dev@protonmail.com>
2021-11-24 06:32:35 +0100

    init Lab6 🤓

---
+++ 6.Registration Form using GET, POST Methods/Lab6_2.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<style>
+        h1 {
+            color: blue;
+        }
+        h2 {
+            color: blueviolet;
+        }
+        p {
+            color: red;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+</style>
